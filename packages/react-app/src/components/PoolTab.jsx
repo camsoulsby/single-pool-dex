038287edc9a748038f1f72b9050a8a37bd1379cf
Cam Soulsby <cameron.soulsby@gmail.com>
2022-07-07 08:06:13 +0700

    fixed issue with pool form

--- packages/react-app/src/components/PoolTab.jsx
+++ packages/react-app/src/components/PoolTab.jsx
@@ -4,8 +4,8 @@ const PoolTab = () => {
     <div className="simple-ui-card">
       <h1>Pool</h1>
       <div className="form-group">
-        <h1 color="white">Test 1</h1>
-        <h1 color="white">Test 2</h1>
+        <h1>Test 1</h1>
+        <h1>Test 2</h1>
 
         <button>Deposit</button>
         <button>Unlock</button>
